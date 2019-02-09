48286a65763672a5423dc7ffbfeb84004bd0b9ef
Fred Campos <fcf.camposx@gmail.com>
2019-02-08 21:33:59 -0400

    VueJS boilerplate

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SlotMachine />
+  </div>
+</template>
+
+<script>
+import SlotMachine from "./components/SlotMachine.vue";
+
+export default {
+  name: "app",
+  components: {
+    SlotMachine
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
